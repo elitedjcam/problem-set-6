5d09507501998095f58ad5ccf78b5148048bc783
Cameron Smith <elitedjcam@gmail.com>
2018-12-07 10:45:46 -0500

    workin on 5

--- js/problem-set-6.js
+++ js/problem-set-6.js
@@ -226,21 +226,13 @@ function drawTriangle() {
  * should do in these instances.
  */
 
+//arc(x, y, radius, startAngle, endAngle, anticlockwise)
 function drawSmileyFace() {
-  var canvas = document.getElementById('canvas');
-    if (canvas.getContext) {
-       var ctx = canvas.getContext('2d');
+  var canvas = document.getElementById('canvas').getContext('2d')
 
-      ctx.beginPath();
-      ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
-      ctx.moveTo(110, 75);
-      ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
-      ctx.moveTo(65, 65);
-      ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
-      ctx.moveTo(95, 65);
-      ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
-      ctx.stroke();
-    }
+
+
+  
 }
 
 /*
